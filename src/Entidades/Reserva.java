52aa305f83dfadbba1c287d4b15bd1f3a949f061
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-06-05 15:56:19 -0300

    exercicio-9: saida do programa.

--- src/Entidades/Reserva.java
+++ src/Entidades/Reserva.java
@@ -2,12 +2,20 @@ package Entidades;
 
 import Excecoes.ExcecaoDominio;
 
+import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reserva {
     private Date dataEntrada;
     private Date dataSaida;
     private int numeroQuarto;
+
     public Reserva(Date dataEntrada, Date dataSaida, int numeroQuarto) throws ExcecaoDominio {
         Date dataHoje = new Date();
         if (dataEntrada.before(dataHoje) || dataSaida.before(dataHoje)){
@@ -28,7 +36,46 @@ public class Reserva {
     public Date getDataSaida() {return dataSaida;}
     public void setDataSaida(Date dataSaida) {this.dataSaida = dataSaida;}
 
-    public static void controleReservas(Date dataEntrada, Date dataSaida) {
+
+    public static long qdadeDiasDeEstadia(Date dataSaida, Date dataEntrada) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        long diferenca = dataSaida.getTime() - dataEntrada.getTime();
+
+        TimeUnit time = TimeUnit.DAYS;
+        long noitesEstadia = time.convert(diferenca, TimeUnit.MILLISECONDS);
+        return noitesEstadia;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva:\n " +
+                "Número do quarto: " + numeroQuarto +
+                "\n Data de entrada: " + dataEntrada +
+                "\n Data de saida=" + dataSaida;
+    }
+
+//    public void imprimirReservas() {
+//        ArrayList<Reserva> reservas = new ArrayList<>();
+//        if (reservas.size() == 0) System.out.println("Lista de reservas vazia.");
+//        else for (int i = 0; i < reservas.size(); i++) {
+//            Reserva reserva = (Reserva) reservas.get(i);
+//            System.out.println("\t" + i + ": " + "Reserva:\n " +
+//                    "\n Número do quarto: " + reserva.getNumeroQuarto() +
+//                    "\n Data de entrada: " + reserva.getDataEntrada() +
+//                    "\n Data de saida: " + reserva.getDataSaida() +
+//                    "\n Período de estadia: " );
+//        }
+//        System.out.println();
+//    }
+
+    public void imprimirInfoReserva() throws ParseException {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("Reserva:\n " +
+                "Número do quarto: " + numeroQuarto +
+                "\n Data de entrada: " + formato.format(dataEntrada) +
+                "\n Data de saida: " + formato.format(dataSaida) +
+                "\n Período da estadia: " + qdadeDiasDeEstadia(dataSaida, dataEntrada) + " noites."
+        );
 
     }
 }
